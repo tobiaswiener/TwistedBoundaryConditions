9e6021c23d1171a05fd026c1a9c4f3a500afa462
tobiaswiener <tobias.wiener@physik.uni-augsburg.de>
2022-02-03 13:44:25 +0100

    impurity added

--- twistedAngle.py
+++ twistedAngle.py
@@ -10,7 +10,7 @@ from utils import printProgressBar
 
 class TwistedAngle:
 
-    def __init__(self, phi_x_lower=0, phi_x_upper=2*np.pi, phi_y_lower=0, phi_y_upper=2*np.pi, nphi_x=100, nphi_y=100, L_x=5, L_y=4, t=1):
+    def __init__(self, phi_x_lower=0, phi_x_upper=2*np.pi, phi_y_lower=0, phi_y_upper=2*np.pi, nphi_x=100, nphi_y=100, L_x=5, L_y=4, t=1, loc_imp=(0,0),e_imp=0):
         self.phi_x_lower = phi_x_lower
         self.phi_x_upper = phi_x_upper
         self.phi_y_lower = phi_y_lower
@@ -20,6 +20,10 @@ class TwistedAngle:
         self.L_x = L_x
         self.L_y = L_y
         self.t = t
+
+
+        self.loc_imp = loc_imp
+        self.e_imp = e_imp
 
     def run_phis(self):
         phi_x_array = np.linspace(self.phi_x_lower, self.phi_x_upper, self.nphi_x)
@@ -39,6 +43,17 @@ class TwistedAngle:
                                                    ("phi_y", phi_y_array),
                                                    ("#evec", np.arange(size_hilbert)),
                                                    ("dim_evec", np.arange(size_hilbert))])
+        eigenvalues_np_imp = np.nan*np.empty((self.nphi_x, self.nphi_y, size_hilbert))
+        self.eigenvalues_xr_imp = xr.DataArray(data=eigenvalues_np_imp, coords=[("phi_x",phi_x_array),
+                                                                   ("phi_y",phi_y_array),
+                                                                   ("#eval",np.arange(size_hilbert))])
+
+        eigenvectors_np_imp = np.nan*np.empty((self.nphi_x, self.nphi_y, size_hilbert, size_hilbert))
+        self.eigenvectors_xr_imp = xr.DataArray(data=eigenvectors_np_imp,
+                                           coords=[("phi_x", phi_x_array),
+                                                   ("phi_y", phi_y_array),
+                                                   ("#evec", np.arange(size_hilbert)),
+                                                   ("dim_evec", np.arange(size_hilbert))])
 
 
         for counter_x, phi_x in enumerate(phi_x_array):
@@ -47,13 +62,31 @@ class TwistedAngle:
                 eval, evec = h.solve()
                 self.eigenvalues_xr[counter_x,counter_y,:] = eval
                 self.eigenvectors_xr[counter_x,counter_y,:,:] = evec
+
+                h_imp = TightBinding_2D(L_x=self.L_x, L_y=self.L_y,phi_x=phi_x,phi_y=phi_y,t=self.t)
+                h_imp.set_impurity(loc_tuple=self.loc_imp, e_imp=self.e_imp)
+                eval_imp, evec_imp = h_imp.solve()
+                self.eigenvalues_xr_imp[counter_x,counter_y,:] = eval_imp
+                self.eigenvectors_xr_imp[counter_x,counter_y,:,:] = evec_imp
             printProgressBar(counter_x + 1, self.nphi_x, prefix='Progress:', suffix='Complete', length=50)
 
+
+
     def plot_eigenvalues_phi_x(self, phi_y=0):
+        fig, axs = plt.subplots(2)
+        fig.suptitle('twisted boundary ')
+
         eigenvalues = self.eigenvalues_xr.isel(phi_y=phi_y)
+        eigenvalues_imp = self.eigenvalues_xr_imp.isel(phi_y=phi_y)
 
         for ev in eigenvalues:
-            plt.scatter(ev["phi_x"].values * np.ones_like(ev.values), ev.values, color='black', s=0.5)
+            axs[0].scatter(ev["phi_x"].values * np.ones_like(ev.values), ev.values, color='black', s=0.5)
+        for ev in eigenvalues_imp:
+            axs[1].scatter(ev["phi_x"].values * np.ones_like(ev.values), ev.values, color='black', s=0.5)
+
+
+        axs[0].set_title("without impurity")
+        axs[1].set_title(f"with impurity at {self.loc_imp}")
 
         plt.show()
 
